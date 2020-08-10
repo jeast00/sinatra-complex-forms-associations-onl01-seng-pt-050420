fd6a4ba5456bd4cfb94670646ca37e723701687b
Jonathan East <jonathan_east@ymail.com>
2020-08-10 16:56:28 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -35,5 +35,6 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
+    redirect "owners/#{@owner.id}"
   end
 end
